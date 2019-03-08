98d64dd44aaccfac8ea3a3b3857c7b1706ad58b2
Kevin Mougammadaly <kevin.mougammadaly@gmail.com>
2019-03-08 12:34:35 +0100

    fix(back): Fix cors request

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const userController = require('./controller/user/user.controller');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
