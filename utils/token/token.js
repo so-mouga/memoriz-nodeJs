4f21ddff4a2890ba7713b8890e55777fa2d6a943
Kevin Mougammadaly <kevin.mougammadaly@gmail.com>
2019-03-26 23:15:09 +0100

    fix(jwt): Change value to return jwt

--- utils/token/token.js
+++ utils/token/token.js
@@ -8,12 +8,12 @@ exports.createToken = details => {
   if (typeof details !== 'object') {
     details = {};
   }
-  if (details.user || typeof details.user == 'object') {
+  if (details || typeof details == 'object') {
     const options = {
       expiresIn: '30d', // 30 days validity
       algorithm: 'RS256',
     };
-    return jwt.sign({ data: details.user }, privateKey, options);
+    return jwt.sign({ data: details }, privateKey, options);
   }
   return null;
 };
@@ -21,16 +21,12 @@ exports.createToken = details => {
 exports.verifyToken = (req, res, next) => {
   const token = getToken(req);
   if (!token) {
-    return res
-      .status(HttpStatus.BAD_REQUEST)
-      .json({ status: false, message: 'need token' });
+    return res.status(HttpStatus.BAD_REQUEST).json({ status: false, message: 'need token' });
   }
 
   jwt.verify(token, publicKey, function(err, decodedToken) {
     if (err || !decodedToken) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ status: false, message: err.message });
+      return res.status(HttpStatus.BAD_REQUEST).json({ status: false, message: err.message });
     }
     req.user = decodedToken;
     next();
